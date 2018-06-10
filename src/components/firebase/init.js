5e81c168a6a218020bf619ced5edb15fd1a2a30c
detautama <detautama11@gmail.com>
2018-06-10 21:49:15 +0800

    Added firebase

---
+++ src/components/firebase/init.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+var config = {
+  apiKey: "AIzaSyCJ0RymudEd6s5EEeUzhl2NTu3nXhQXp8A",
+  authDomain: "udemy-ninja-chat-3bfc3.firebaseapp.com",
+  databaseURL: "https://udemy-ninja-chat-3bfc3.firebaseio.com",
+  projectId: "udemy-ninja-chat-3bfc3",
+  storageBucket: "udemy-ninja-chat-3bfc3.appspot.com",
+  messagingSenderId: "947117565154"
+};
+const firebaseApp = firebase.initializeApp(config);
+
+export default firebaseApp.firestore()
